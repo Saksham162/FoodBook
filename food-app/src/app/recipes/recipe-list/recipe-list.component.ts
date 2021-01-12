729165ca0aaf3c3740ed1636ddabfdec4cf8ffac
Saksham <sakshambhatheja@gmail.com>
2021-01-12 19:37:30 +0530

    my Project work9

---
+++ food-app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe} from '../recipe.model';
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe','This is simply a test','https://www.inspiredtaste.net/wp-content/uploads/2019/06/Mediterranean-White-Bean-Salad-Recipe-1-1200.jpg'),
+    
+    new Recipe('A Test Recipe','This is simply a test','https://www.inspiredtaste.net/wp-content/uploads/2019/06/Mediterranean-White-Bean-Salad-Recipe-1-1200.jpg')
+  
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
